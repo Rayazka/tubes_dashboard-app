dbb37d9b2ae9ee7ffeb48d17c93ce46f336126a2
Ozz__ <rayazkaaris@gmail.com>
2025-05-27 01:33:23 +0700

    first commit

---
+++ src/test/java/com/tubes/dashboard_app/DashboardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubes.dashboard_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
